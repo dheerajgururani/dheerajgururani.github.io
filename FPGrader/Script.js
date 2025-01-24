9295ada81acf1db8340bd199260585b0475c2ce8
Dheeraj Gururani <91014697+dheerajgururani@users.noreply.github.com>
2025-01-24 17:18:38 +0100

    Update Script.js

--- FPGrader/Script.js
+++ FPGrader/Script.js
@@ -1,44 +1,18 @@
-
-
-
-// let url = 'https://gsx2json.com/api?id=1hFnrv9y1ZTce0tDlWuXTJ9wwrwvFB_rw54-9z4lA8xI&sheet=Sheet1&columns=False';
-
-
-
-
 $("#btn").click(function () {
-    // document.getElementById("info").style.display = "none";
-    // let x = document.getElementById('link').value.split('/d/')[1].split('/edit')[0];
-    // url = 'https://gsx2json.com/api?id=' + x + '&sheet=Sheet1&columns=False';
     document.getElementById("info").style.display = "none";
     let x = document.getElementById('link').value;
     if (x != "") {
         x = x.split('/d/')[1].split('/edit')[0];
-        let url = 'https://gsx2json.com/api?id=' + x + '&sheet=Sheet1&columns=False';
+        let url = 'https://gsx2json.com/api?id=' + x + '&amp;sheet=Sheet1&amp;columns=False';
     }
 
     console.log(url);
     $.getJSON(url, function (data) {
 
-
-        // Find all the rows with same FullName and sum the points of HW1,HW2 and so on in a variable HWPointsArray, every object
-        // in the array will have the FullName and the sum of the points for that student
         data = data.rows;
         let TotalPointsArray = [];
-        // let newObject = {};
 
-        //Data contains an array of objects, each object is a row in the spreadsheet
-        // evry object has a property called FullName, we will use this to find all the rows with the same FullName
-        // every object has a Assigment property, which is HW1 , HW2 ... HW10 or PT1, Pt2 ... PT10
-        // we need to sum all the PT and HW points for each student and put them in an array of objects
-        // each object will have fullName , HWsum and PTsum 
-        //implement your code here
-
-        // loop through the data array
-        // for each object in the array, check if the FullName is already in the TotalPointsArray
-        // if it is not, add a new object to the TotalPointsArray with the FullName and the points for that Assignments
-        // if it is, find the object in the TotalPointsArray with the same FullName , then check if the Assignments is HW or PT and add the points to the correct property
-
+        // Loop through the data array to sum the points
         for (let i = 0; i < data.length; i++) {
             let FullName = data[i].FullName;
             let Assignments = data[i].Assignments;
@@ -48,10 +22,7 @@ $("#btn").click(function () {
             for (let j = 0; j < TotalPointsArray.length; j++) {
                 if (TotalPointsArray[j].FullName == FullName) {
                     found = true;
-                    if (Assignments.includes("HW")) {
-
-                        TotalPointsArray[j].HWPointsArray.push(Points);
-                    } else if (Assignments.includes("PT")) {
+                    if (Assignments.includes("PT")) {
                         TotalPointsArray[j].PTPointsArray.push(Points);
                     } else if (Assignments.includes("MiniQuiz")) {
                         TotalPointsArray[j].MiniQuizPoints.push(Points);
@@ -72,7 +43,6 @@ $("#btn").click(function () {
             if (!found) {
                 let newObject = {
                     FullName: FullName,
-                    HWPointsArray: [],
                     PTPointsArray: [],
                     MiniQuizPoints: 0,
                     MidtermQuizPoints: 0,
@@ -80,11 +50,8 @@ $("#btn").click(function () {
                     MidtermPoints: 0,
                     EndtermPoints: 0,
                     TheoreticalQuizPoints: 0
-
-                }
-                if (Assignments.includes("HW")) {
-                    newObject.HWPointsArray.push(Points);
-                } else if (Assignments.includes("PT")) {
+                };
+                if (Assignments.includes("PT")) {
                     newObject.PTPointsArray.push(Points);
                 } else if (Assignments.includes("MiniQuiz")) {
                     newObject.MiniQuizPoints = Points;
@@ -96,68 +63,58 @@ $("#btn").click(function () {
                     newObject.MidtermPoints = Points;
                 } else if (Assignments.includes("EndtermPoints")) {
                     newObject.EndtermPoints = Points;
-
                 }
                 else if (Assignments.includes("TheoreticalQuiz")) {
                     newObject.TheoreticalQuizPoints = Points;
                 }
                 TotalPointsArray.push(newObject);
             }
-
-
         }
 
-        console.log(TotalPointsArray);
-
-        // loop through the TotalPointsArray , sort the HWPointsArray and PTPointsArray in ascescending order and sum all but the first 2 values in each array, put the sum in a new property called HWPoints and PTPoints
-        // implement your code here
-
+        // Sort and calculate points for PT
         for (let i = 0; i < TotalPointsArray.length; i++) {
-            let HWPointsArray = TotalPointsArray[i].HWPointsArray;
             let PTPointsArray = TotalPointsArray[i].PTPointsArray;
-            HWPointsArray.sort(function (a, b) {
-                return a - b
-            });
-            PTPointsArray.sort(function (a, b) {
-                return a - b
-            });
-            let HWPoints = 0;
+            PTPointsArray.sort(function (a, b) { return a - b });
             let PTPoints = 0;
-            for (let j = 2; j < HWPointsArray.length; j++) {
-                HWPoints += HWPointsArray[j];
-            }
             for (let j = 2; j < PTPointsArray.length; j++) {
                 PTPoints += PTPointsArray[j];
             }
-            TotalPointsArray[i].HWPoints = HWPoints;
             TotalPointsArray[i].PTPoints = PTPoints;
         }
 
+        // Function to calculate the final grade
+        function getGradeFinal(object) {
+            let PTPoints = object.PTPoints;
+            let MiniQuizPoints = object.MiniQuizPoints;
+            let MidtermQuizPoints = object.MidtermQuizPoints;
+            let EndtermQuizPoints = object.EndtermQuizPoints;
+            let MidtermPoints = object.MidtermPoints;
+            let EndtermPoints = object.EndtermPoints;
+            let TheoreticalQuizPoints = object.TheoreticalQuizPoints;
 
-        console.log(TotalPointsArray);
+            // Check if any grade condition is failed
+            if (PTPoints < 400 || MiniQuizPoints < 20 || TheoreticalQuizPoints < 20 || MidtermPoints < 50 || EndtermPoints < 50) {
+                return "FAILED";
+            } else {
+                // Calculate X based on the new criteria (no HWPoints)
+                let X = ((MidtermQuizPoints + EndtermQuizPoints) / 100) * 10 + ((MidtermPoints) / 100) * 40 + ((EndtermPoints) / 100) * 40;
 
-        //make a function that takes an object of the TotalPointsArray and returns a grade based on the following criteria
-        // if the PTPoints are less than 400, the grade is 1 . otherwise if the HWPoints is between 0-300 grade 2 , 301 - 500 grade 3
-        // 500-700 grade 4 and 700 and above grade 5
-
-        function getGrade(object) {
-            if (object.PTPoints < 400) {
-                return 1;
-            } else if (object.HWPoints >= 0 && object.HWPoints <= 300) {
-                return 2;
-            } else if (object.HWPoints >= 301 && object.HWPoints <= 500) {
-                return 3;
-            } else if (object.HWPoints >= 501 && object.HWPoints <= 700) {
-                return 4;
-            } else if (object.HWPoints >= 701) {
-                return 5;
+                // Assign grades based on X value
+                if (X > 85) {
+                    return 5;
+                } else if (X >= 70) {
+                    return 4;
+                } else if (X >= 60) {
+                    return 3;
+                } else if (X >= 50) {
+                    return 2;
+                } else {
+                    return "FAILED";
+                }
             }
         }
 
-
-
-        // loop through the TotalPointsArray and add a new property called Grade and assign it the value returned by the getGrade function
-
+        // Loop through the TotalPointsArray to assign grades
         for (let i = 0; i < TotalPointsArray.length; i++) {
             TotalPointsArray[i].Grade = getGradeFinal(TotalPointsArray[i]);
 
@@ -167,164 +124,7 @@ $("#btn").click(function () {
             let cell2 = row.insertCell(1);
             cell1.innerHTML = TotalPointsArray[i].FullName;
             cell2.innerHTML = TotalPointsArray[i].Grade;
-
         }
-
-        // 
-
-
-
-
-
-
-
-
-
 
     });
 });
-
-function getGradeFinal(object) {
-
-    let HWPoints = object.HWPoints;
-    let PTPoints = object.PTPoints;
-    let MiniQuizPoints = object.MiniQuizPoints;
-    let MidtermQuizPoints = object.MidtermQuizPoints;
-    let EndtermQuizPoints = object.EndtermQuizPoints;
-    let MidtermPoints = object.MidtermPoints;
-    let EndtermPoints = object.EndtermPoints;
-    let TheoreticalQuizPoints = object.TheoreticalQuizPoints;
-
-    if (MidtermPoints > 100) {
-        MidtermPoints = 100;
-    }
-    if (EndtermPoints > 100) {
-        EndtermPoints = 100;
-    }
-    console.log(TheoreticalQuizPoints);
-
-    if (isNaN(TheoreticalQuizPoints)) {
-        TheoreticalQuizPoints = MidtermQuizPoints + EndtermQuizPoints;
-    }
-    console.log(TheoreticalQuizPoints);
-
-
-    if (HWPoints < 400 || PTPoints < 400 || MiniQuizPoints < 20 || TheoreticalQuizPoints < 20 || MidtermPoints < 50 || EndtermPoints < 50) {
-        return "FAILED";
-    } else {
-
-        // let X = (((HWPoints / 800) * 100) * 0.15) + ((MidtermQuizPoints + EndtermQuizPoints) * 0.15) + (MidtermPoints * 0.35) + (EndtermPoints * 0.35);
-        // X is max 100 average of HWPoints is 15% of X, sum of MidtermQuizPoints and EndtermQuizPoints is 15% of X, MidtermPoints is 35% of X, EndtermPoints is 35% of X
-        // calculate X 
-        // implement your code here
-        let X = (((HWPoints / 800) * 15) + (( TheoreticalQuizPoints / 40) * 15) + ((MidtermPoints / 100) * 35) + ((EndtermPoints / 100) * 35));
-        // console.log(X);
-        // console.log((HWPoints / 800) * 15);
-        // console.log(((MidtermQuizPoints + +EndtermQuizPoints) / 50) * 15);
-        // console.log((MidtermPoints / 100) * 35);
-        // console.log((EndtermPoints / 100) * 35);
-
-        if (X > 85) {
-            return 5;
-        } else if (X >= 70) {
-            return 4;
-        } else if (X >= 60) {
-            return 3;
-        } else if (X >= 50) {
-            return 2;
-        } else if (X < 50) {
-            return "FAILED";
-        }
-    }
-}
-
-function calculateGrade() {
-    // let FullName = document.getElementById("FullName").value;
-    let HWPoints = document.getElementById("HWPoints").value;
-    let PTPoints = document.getElementById("PTPoints").value;
-    // let HWPointsArray = HWPoints.split(",");
-    // let PTPointsArray = PTPoints.split(",");
-    // let HWPointsSum = 0;
-    // let PTPointsSum = 0;
-    let MiniQuizPoints = document.getElementById("MiniQuizPoints").value;
-    let MidtermQuizPoints = document.getElementById("MidtermQuizPoints").value;
-    let EndtermQuizPoints = document.getElementById("EndtermQuizPoints").value;
-    let MidtermPoints = document.getElementById("MidtermPoints").value;
-    let EndtermPoints = document.getElementById("EndtermPoints").value;
-    let TheoreticalQuizPoints = document.getElementById("TheoreticalQuizPoints").value;
-
-
-    // for (let i = 0; i < HWPointsArray.length; i++) {
-    //     HWPointsSum += parseInt(HWPointsArray[i]);
-    // }
-    // for (let i = 0; i < PTPointsArray.length; i++) {
-    //     PTPointsSum += parseInt(PTPointsArray[i]);
-    // }
-    let object = {
-        // FullName: FullName,
-        HWPoints: parseInt(HWPoints),
-        PTPoints: parseInt(PTPoints),
-        MidtermPoints: parseInt(MidtermPoints),
-        EndtermPoints: parseInt(EndtermPoints),
-        MiniQuizPoints: parseInt(MiniQuizPoints),
-        MidtermQuizPoints: parseInt(MidtermQuizPoints),
-        EndtermQuizPoints: parseInt(EndtermQuizPoints),
-        TheoreticalQuizPoints: parseInt(TheoreticalQuizPoints)
-
-
-    }
-    // console.log(object);
-    let grade = getGradeFinal(object);
-    document.getElementById("grade").innerHTML = grade;
-}
-
-
-
-
-
-
-document.getElementById("getGrade").addEventListener("click", calculateGrade);
-
-document.getElementById("manual").addEventListener("click", function () {
-    document.getElementById("information").classList.remove("py-5");
-    document.getElementById("information").classList.remove("mt-4");
-    document.getElementById("information").classList.add("mt-0");
-
-    document.getElementById("information").classList.add("py-3");
-    document.getElementById("selection").style.display = "none";
-    document.getElementById("AutoData").style.display = "none";
-    document.getElementById("ManualData").style.display = "block";
-
-});
-
-document.getElementById("auto").addEventListener("click", function () {
-    document.getElementById("information").classList.remove("py-5");
-    document.getElementById("information").classList.remove("mt-4");
-    document.getElementById("information").classList.add("mt-0");
-
-    document.getElementById("information").classList.add("py-3");
-    document.getElementById("selection").style.display = "none";
-    document.getElementById("ManualData").style.display = "none";
-    document.getElementById("AutoData").style.display = "block";
-});
-
-// display element with id info when user clicks element with id help
-document.getElementById("help").addEventListener("click", function () {
-    document.getElementById("info").style.display = "block";
-});
-
-//on clicking FpGrader , refresh the page
-document.getElementById("FPGrader").addEventListener("click", function () {
-    // return to index.html
-    window.location.href = "index.html";
-});
-
-
-
-
-
-
-
-
-
-
